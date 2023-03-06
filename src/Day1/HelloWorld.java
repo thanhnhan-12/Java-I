9e18ba46a01cef47d94e6a1dacbffdea88a29cb2
Thanh Nhan <nguyenthanhnhan12701@gmail.com>
2023-03-06 23:13:02 +0700

    Java I

---
+++ src/Day1/HelloWorld.java
@@ -0,0 +1,18 @@
+package Day1;
+
+public class HelloWorld {
+  
+  public static void main(String[] args) {
+    int age = 20;
+    String name = "Java Programming";
+    String comment = "\\";
+    System.out.println("\nAge is " + age + " years old");
+    System.out.println("\nName is " + name + "\n");
+
+    System.out.println("Dau" + " // " +  "dung de in ghi chu trong Java" + comment);
+
+    System.out.println("Ngoac kep " + " \t - ");
+
+  }
+
+}
